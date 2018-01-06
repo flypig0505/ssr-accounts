e8fd927c9fb94e538ec7382c12ff09dc04c54d84
gfw-breaker <lixiaoming0513@gmail.com>
2018-01-06 20:23:32 +0800

    add ntdtv

---
+++ resources/dj_jquery.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+jq_home="/usr/local/nginx/html/ajax/libs/jquery/"
+versions="3.2.1, 3.2.0, 3.1.1, 3.1.0, 3.0.0, 2.2.4, 2.2.3, 2.2.2, 2.2.1, 2.2.0, 2.1.4, 2.1.3, 2.1.1, 2.1.0, 2.0.3, 2.0.2, 2.0.1, 2.0.0, 1.12.4, 1.12.3, 1.12.2, 1.12.1, 1.12.0, 1.11.3, 1.11.2, 1.11.1, 1.11.0, 1.10.2"
+
+function format_url(){
+	local ver=$1
+	local url=http://ajax.googleapis.com/ajax/libs/jquery/${ver}/jquery.min.js
+	echo $url
+}
+
+cur_dir=$(pwd)
+mkdir -p $jq_home
+cd $jq_home
+
+for v in $(echo $versions | sed 's/,/ /g'); do
+	url=$(format_url $v)
+	mkdir $v && cd $v
+	wget $url
+	cd ..
+done 
+
+cd $cur_dir
+
+
--- resources/install.sh
+++ resources/install.sh
@@ -9,19 +9,13 @@ tar xzf rarlinux*; (cd rar; make)
 
 
 ## configure nginx
-jquery_path=/usr/local/nginx/html/ajax/libs/jquery/1.11.3
-mkdir -p $jquery_path
-cp jquery.min.js $jquery_path
-
 ip_addr=$(ifconfig | grep "inet addr" | sed -n 1p | cut -d':' -f2 | cut -d' ' -f1)
 sed -i "/#epochtimes#/a\\\\t\\t\\tsub_filter www.epochtimes.com $ip_addr;" nginx.conf
 sed -i "/#epochtimes#/a\\\\t\\t\\tsub_filter i.epochtimes.com $ip_addr;" nginx.conf
 sed -i "/#epochtimes#/a\\\\t\\t\\tsub_filter https://ajax.googleapis.com http://$ip_addr;" nginx.conf
-sed -i "/#falundafa#/a\\\\t\\t\\tsub_filter www.falundafa.org $ip_addr:8000;" nginx.conf
-sed -i "/#falundafa#/a\\\\t\\t\\tsub_filter www.minghui.org $ip_addr:8080;" nginx.conf
-sed -i "/#minghui#/a\\\\t\\t\\tsub_filter www.minghui.org $ip_addr:8080;" nginx.conf
-sed -i "/#minghui#/a\\\\t\\t\\tsub_filter stats.minghui.org $ip_addr:8080;" nginx.conf
-sed -i "/#minghui#/a\\\\t\\t\\tsub_filter gb.falundafa.org $ip_addr:8000;" nginx.conf
+sed -i "/#ntdtv#/a\\\\t\\t\\tsub_filter www.ntdtv.com $ip_addr:8000;" nginx.conf
+sed -i "/#nvtdv#/a\\\\t\\t\\tsub_filter https://ajax.googleapis.com http://$ip_addr;" nginx.conf
+sed -i "/#nvtdv#/a\\\\t\\t\\tsub_filter http://ajax.googleapis.com http://$ip_addr;" nginx.conf
 sed -i "/#google#/a\\\\t\\t\\tsub_filter https://www.google.com http://$ip_addr:8888;" nginx.conf
 sed -i "/#google#/a\\\\t\\t\\tsub_filter https://id.google.com http://$ip_addr:8888;" nginx.conf
 mv nginx.conf /usr/local/nginx/conf/nginx.conf
@@ -29,6 +23,7 @@ mv nginx /etc/init.d/nginx
 chmod +x /etc/init.d/nginx
 chkconfig nginx on
 service nginx start
+
 
 ## set links
 page_path=/usr/local/nginx/html/info
@@ -39,15 +34,17 @@ sed -i "s/type/ss/" $page_path/ss.html
 sed "s/localhost/$ip_addr/g" link.html > $page_path/ssr.html
 sed -i "s/type/ssr/" $page_path/ssr.html
 
-jquery_path=/usr/local/nginx/html/ajax/libs/jquery/1.11.3
-mkdir -p $jquery_path
-mv jquery.min.js $jquery_path
+
+## download js libs
+./dj_jquery.sh
+
 
 ## install ssr & bbr
 unzip shadowsocksr.zip
 mv shadowsocksr /usr/local/shadowsocksr
 chmod +x ssr.sh && bash ssr.sh
 chmod +x bbr.sh && bash bbr.sh
+
 
 ## enable ssr service
 mv ssr /etc/init.d/ssr
