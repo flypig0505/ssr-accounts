1cf19255f38bdf2eb21b5479617bc63bba09fa72
gfw-breaker <lixiaoming0513@gmail.com>
2018-01-16 21:34:15 +0800

    migrate nginx related content

--- resources/sanitize_conf.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-local_ip=$(ifconfig | grep "inet addr" | sed -n 1p | cut -d':' -f2 | cut -d' ' -f1)
-
-for conf in $(ls *.nginx.conf); do
-	sed -i "%s/$local_ip/local_server_ip/g" $conf
-done
-
-
