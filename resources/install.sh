4d0c9d50979c9f8fcca40294e9a46a17809cb807
root <root@web01.(none)>
2018-01-24 10:45:06 +0800

    change templates

--- resources/install.sh
+++ resources/install.sh
@@ -8,18 +8,24 @@ yum install -y vim gcc gcc-c++ wget unzip net-tools openssl-devel python jq qren
 ## set links
 page_path=/usr/local/nginx/html/info
 mkdir -p $page_path
+video_path=/usr/local/nginx/html/content/videos
+mkdir -p $video_path
+
+local_ip=$(ifconfig | grep "inet addr" | sed -n 1p | cut -d':' -f2 | cut -d' ' -f1)
 sed "s/localhost/$local_ip/g" link.html > $page_path/ss.html
 sed -i "s/type/ss/" $page_path/ss.html
 
 sed "s/localhost/$local_ip/g" link.html > $page_path/ssr.html
 sed -i "s/type/ssr/" $page_path/ssr.html
 
+sed "s/localhost/$local_ip/g" videos.html > $video_path/index.html
+
 
 ## install ssr & bbr
 unzip shadowsocksr.zip
-cp shadowsocksr /usr/local/shadowsocksr
+cp -R shadowsocksr /usr/local/shadowsocksr
 chmod +x ssr.sh && bash ssr.sh
-#chmod +x bbr.sh && bash bbr.sh
+chmod +x bbr.sh && bash bbr.sh
 
 
 ## enable ssr service
